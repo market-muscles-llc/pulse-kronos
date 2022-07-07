e0ae2fdb27756e5da067363f2b2d141c3c0efda1
Adrian Gonzales <adrian@virexmachina.com>
2022-07-07 10:08:36 -0500

    Swap Cal branding

--- apps/web/components/ui/PoweredByCal.tsx
+++ apps/web/components/ui/PoweredByCal.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 
 import { useIsEmbed } from "@calcom/embed-core/embed-iframe";
-import { POWERED_BY_URL } from "@calcom/lib/constants";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 
 const PoweredByCal = () => {
@@ -9,23 +8,23 @@ const PoweredByCal = () => {
   const isEmbed = useIsEmbed();
   return (
     <div className={"p-1 text-center text-xs sm:text-right" + (isEmbed ? " max-w-3xl" : "")}>
-      <Link href={POWERED_BY_URL}>
+      <Link href="https://movewithpulse.com/?utm_source=embed&amp;utm_medium=powered-by-button">
         <a target="_blank" className="text-bookinglight opacity-50 hover:opacity-100 dark:text-white">
           {t("powered_by")}{" "}
           {
             // eslint-disable-next-line @next/next/no-img-element
             <img
-              className="relative -mt-px inline h-[10px] w-auto dark:hidden"
-              src="/cal-logo-word.svg"
-              alt="Cal.com Logo"
+              className="relative -mt-px inline h-6 w-auto dark:hidden"
+              src="/pulse-logo.svg"
+              alt="Move With Pulse Logo"
             />
           }
           {
             // eslint-disable-next-line @next/next/no-img-element
             <img
-              className="relativ -mt-px hidden h-[10px] w-auto dark:inline"
-              src="/cal-logo-word-dark.svg"
-              alt="Cal.com Logo"
+              className="relative -mt-px hidden h-6 w-auto dark:inline"
+              src="/pulse-logo-light.svg"
+              alt="Move With Pulse Logo"
             />
           }
         </a>

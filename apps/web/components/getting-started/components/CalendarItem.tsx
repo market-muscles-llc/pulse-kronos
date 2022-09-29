55ba25871ee5bc51904266c9d48a3a2e70da28ad
Adrian Gonzales <adrian@virexmachina.com>
2022-09-29 17:08:05 -0500

    Google Connect Button on Signup

--- apps/web/components/getting-started/components/CalendarItem.tsx
+++ apps/web/components/getting-started/components/CalendarItem.tsx
@@ -10,9 +10,28 @@ interface ICalendarItem {
   type: App["type"];
 }
 
+function ConnectButtonContent({ type }) {
+  const { t } = useLocale();
+
+  if (type === "google_calendar") {
+    return (
+      <>
+        <img
+          className="h-[34px] w-auto"
+          height="34px"
+          alt="Sign in with Google"
+          title="Sign in with Google"
+          src="/btn_google_signin_light_normal_web.png"
+        />
+      </>
+    );
+  }
+
+  return <span className="rounded-md py-9 px-4 text-sm font-bold">{t("connect")}</span>;
+}
+
 const CalendarItem = (props: ICalendarItem) => {
   const { title, imageSrc, type } = props;
-  const { t } = useLocale();
   return (
     <div className="flex flex-row items-center p-5">
       <img src={imageSrc} alt={title} className="h-8 w-8" />
@@ -30,8 +49,8 @@ const CalendarItem = (props: ICalendarItem) => {
               document.cookie = `return-to=${window.location.href};path=/;max-age=3600;SameSite=Lax`;
               buttonProps && buttonProps.onClick && buttonProps?.onClick(event);
             }}
-            className="ml-auto rounded-md border border-gray-200 py-[10px] px-4 text-sm font-bold">
-            {t("connect")}
+            className="ml-auto rounded-none px-0 py-0">
+            <ConnectButtonContent type={type} />
           </Button>
         )}
       />

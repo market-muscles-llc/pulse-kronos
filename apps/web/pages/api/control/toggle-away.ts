72066355399c5e20701ca408e1d8f82979ca145a
Adrian Gonzales <adrian@virexmachina.com>
2022-10-03 15:55:58 -0500

    Private control API endpoints, remove sentry

---
+++ apps/web/pages/api/control/toggle-away.ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { asNumberOrThrow } from "@lib/asStringOrNull";
+import prisma from "@lib/prisma";
+
+/**
+ *
+ */
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Invalid method" });
+    return;
+  }
+
+  const bearerToken = req.headers.authorization;
+  if ("Bearer " + process.env.CONTROL_API_KEY !== bearerToken) {
+    res.status(401).json({ message: "Not authenticated: " + bearerToken });
+    return;
+  }
+
+  const data = req.body;
+  const { id } = data;
+
+  if (!id) {
+    res.status(400).json({ message: "Invalid input" });
+    return;
+  }
+
+  const user = await prisma.user.findUniqueOrThrow({
+    where: {
+      id: asNumberOrThrow(id),
+    },
+    select: {
+      id: true,
+      away: true,
+    },
+  });
+
+  if (!user) {
+    res.status(404).json({ message: "User not found" });
+    return;
+  }
+
+  await prisma.user.update({
+    where: {
+      id: user?.id,
+    },
+    data: {
+      away: !user?.away,
+    },
+  });
+
+  res.status(200).json({ message: "Set Status", away: !user.away });
+};
+
+export default handler;
---
+++ apps/web/pages/api/control/upsert.ts
@@ -0,0 +1,106 @@
+import { IdentityProvider } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { asNumberOrThrow } from "@lib/asStringOrNull";
+import { hashPassword } from "@lib/auth";
+import prisma from "@lib/prisma";
+
+/**
+ *
+ */
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Invalid method" });
+    return;
+  }
+
+  const bearerToken = req.headers.authorization;
+  if ("Bearer " + process.env.CONTROL_API_KEY !== bearerToken) {
+    res.status(401).json({ message: "Not authenticated: " + bearerToken });
+    return;
+  }
+
+  const data = req.body;
+  const { id, email, username, password, fqdn } = data;
+  let { name, timezone } = data;
+
+  if (!email || email.trim().length < 3) {
+    res.status(422).json({ message: "Invalid email" });
+    return;
+  }
+
+  if (!password || password.trim().length < 7) {
+    res.status(422).json({ message: "Invalid input" });
+    return;
+  }
+
+  if (!timezone) {
+    timezone = "America/New York";
+  }
+
+  if (!name) {
+    name = username;
+  }
+
+  const hashedPassword = await hashPassword(password);
+
+  if (id) {
+    const user = await prisma.user.upsert({
+      where: { id: asNumberOrThrow(id) },
+      update: {
+        email: email,
+        password: hashedPassword,
+        timeZone: timezone,
+        metadata: {
+          fqdn: fqdn,
+        },
+      },
+      create: {
+        name,
+        username,
+        email: email,
+        password: hashedPassword,
+        timeZone: timezone,
+        emailVerified: new Date(Date.now()),
+        identityProvider: IdentityProvider.CAL,
+        plan: "PRO",
+        theme: "light",
+        metadata: {
+          fqdn: fqdn,
+        },
+      },
+    });
+
+    res.status(201).json({ message: "Upserted user", user: user });
+  } else {
+    const user = await prisma.user.upsert({
+      where: { email: email },
+      update: {
+        email: email,
+        password: hashedPassword,
+        timeZone: timezone,
+        metadata: {
+          fqdn: fqdn,
+        },
+      },
+      create: {
+        name,
+        username,
+        email: email,
+        password: hashedPassword,
+        timeZone: timezone,
+        emailVerified: new Date(Date.now()),
+        identityProvider: IdentityProvider.CAL,
+        plan: "PRO",
+        theme: "light",
+        metadata: {
+          fqdn: fqdn,
+        },
+      },
+    });
+
+    res.status(201).json({ message: "Created user", user: user });
+  }
+};
+
+export default handler;

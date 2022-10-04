ca13786d4fbf2b3a3ad16cf944390d7f6656afea
Adrian Gonzales <adrian@virexmachina.com>
2022-10-04 17:12:28 -0500

    Override webhook query

--- packages/features/webhooks/utils/getWebhooks.tsx
+++ packages/features/webhooks/utils/getWebhooks.tsx
@@ -1,6 +1,5 @@
 import { WebhookTriggerEvents } from "@prisma/client";
-
-import prisma from "@calcom/prisma";
+import { Webhook } from "@prisma/client";
 
 export type GetSubscriberOptions = {
   userId: number;
@@ -10,35 +9,27 @@ export type GetSubscriberOptions = {
 
 const getWebhooks = async (options: GetSubscriberOptions) => {
   const { userId, eventTypeId } = options;
-  const allWebhooks = await prisma.webhook.findMany({
-    where: {
-      OR: [
-        {
-          userId,
-        },
-        {
-          eventTypeId,
-        },
-      ],
-      AND: {
-        eventTriggers: {
-          has: options.triggerEvent,
-        },
-        active: {
-          equals: true,
-        },
+  return new Promise<Webhook[]>((resolve) => {
+    const controlWebhooks: Webhook[] = [
+      {
+        id: "control",
+        subscriberUrl: process.env.CONTROL_WEBHOOK_ENDPOINT,
+        payloadTemplate: null,
+        appId: null,
+        secret: process.env.CONTROL_WEBHOOK_SECRET,
+        userId: userId,
+        eventTypeId: eventTypeId,
+        active: true,
+        eventTriggers: [
+          WebhookTriggerEvents.BOOKING_CANCELLED,
+          WebhookTriggerEvents.BOOKING_CREATED,
+          WebhookTriggerEvents.BOOKING_RESCHEDULED,
+        ],
+        createdAt: new Date(),
       },
-    },
-    select: {
-      id: true,
-      subscriberUrl: true,
-      payloadTemplate: true,
-      appId: true,
-      secret: true,
-    },
+    ];
+    resolve(controlWebhooks);
   });
-
-  return allWebhooks;
 };
 
 export default getWebhooks;
